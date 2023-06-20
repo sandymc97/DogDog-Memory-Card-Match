c1e4306908ec288c0872f95cfdd16029623fad03
Sandy <sandyahmedy7@gmail.com>
2023-06-20 16:59:01 -0500

    add css

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log("hi")
+// mvp have a grid to hold the cards
+// when the player picks a card or plays- initialize game
+// when first card is picked, needs to stay up until 2nd card is flipped 
+// cards need to flip back when no match and display wrong or try again
+// if they are a match, stay up
+// if all cards matched, you have a winner
+// button to reset the board and reshuffle cards
+
+
+
+
+
+// declare variables 
+       //variables needed for 1st chosen card and second chosen card
+
+       //variables needed for all the cards?
+
+       //variable needed for timer?
+
+
+// reference html elements
+
+
+
+//event listeners needed for clicks on cards and click on play button
+
+
+
+//functions
+     //function needed to put cards in random order 
+
+     //functions needed to flip cards 
+
+     //function needed to check for match
+
+     //function needed to flip card back if no match?
+
+    //function needed to use timer
+
+    //function needed to reset game? 
